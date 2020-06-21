63a7d04d0fc7345c37419feafd91537dd09d0a94
A-l-y-x <57370461+A-l-y-x@users.noreply.github.com>
2020-06-20 22:59:19 -0700

    Updates

---
+++ precache-manifest.937f5b2362b2e8ffc1c60f7060cb592e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969c706d8dac6731105b6b04ac1e10ff",
+    "url": "/Buffalo-Prepare-to-Sell/index.html"
+  },
+  {
+    "revision": "d1411aaa55de5d31551b",
+    "url": "/Buffalo-Prepare-to-Sell/static/css/main.9b7b952f.chunk.css"
+  },
+  {
+    "revision": "3509ea5c8fbf38c9d283",
+    "url": "/Buffalo-Prepare-to-Sell/static/js/2.b1f5f18d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Buffalo-Prepare-to-Sell/static/js/2.b1f5f18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1411aaa55de5d31551b",
+    "url": "/Buffalo-Prepare-to-Sell/static/js/main.c8f69af1.chunk.js"
+  },
+  {
+    "revision": "b4e62388883f1757d4cf",
+    "url": "/Buffalo-Prepare-to-Sell/static/js/runtime-main.d3a3c970.js"
+  },
+  {
+    "revision": "e887fbacc9183c2aa92f2704a4723599",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/CatalinaAnacapa.e887fbac.ttf"
+  },
+  {
+    "revision": "47bb6872fbee7092e58f959771d9f52d",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/arrow-down.47bb6872.svg"
+  },
+  {
+    "revision": "80c3efeb191cbeebef8adeb7f4d9743c",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/bg.80c3efeb.svg"
+  },
+  {
+    "revision": "eb7662d318919ad18c8c40ad92cf2c6f",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/diamond-row.eb7662d3.svg"
+  },
+  {
+    "revision": "e5fe3f7541cbdfd8fb825f8a31363bdb",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p10.e5fe3f75.jpg"
+  },
+  {
+    "revision": "d773a9bb420f94c2b40acb95d77c497b",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p3.d773a9bb.jpg"
+  },
+  {
+    "revision": "4eb055e10e081cd4269c5379ce0671b4",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p4.4eb055e1.jpg"
+  },
+  {
+    "revision": "bb096cec5d6a5dccc016b154edfa3753",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p5.bb096cec.jpg"
+  },
+  {
+    "revision": "a43130b0465acbf5daebde554d90c750",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p6.a43130b0.jpg"
+  },
+  {
+    "revision": "9a0faed519658d4bfcaa05e5a2b4ea21",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p7.9a0faed5.jpg"
+  },
+  {
+    "revision": "ca045cea4227a7868d504064b5499a26",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p8.ca045cea.jpg"
+  },
+  {
+    "revision": "57ce6e261a494e26f833bef7c8a0aada",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/p9.57ce6e26.jpg"
+  },
+  {
+    "revision": "c213654e2284b0b822cf57ff53e8c518",
+    "url": "/Buffalo-Prepare-to-Sell/static/media/welcomeCovid.c213654e.svg"
+  }
+]);
